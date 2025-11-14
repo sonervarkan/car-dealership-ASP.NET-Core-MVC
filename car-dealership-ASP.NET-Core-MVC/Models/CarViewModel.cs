3f039576015f05f5770e10e8f331b74fedd9f904
sonervarkan <sonervarkan1455@yahoo.com>
2025-11-14 11:51:30 +0300

    Refactor CarViewModel properties and comments

--- car-dealership-ASP.NET-Core-MVC/Models/CarViewModel.cs
+++ car-dealership-ASP.NET-Core-MVC/Models/CarViewModel.cs
@@ -9,11 +9,11 @@ namespace car_dealership_ASP.NET_Core_MVC.Models
         public Cars? CarVMAdd { get; set; }
         public List<Users>? Users { get; set; }
 
-        //  Resim yükleme alanı
+       
         public List<IFormFile>? CarImages { get; set; }
 
         // FILTER  
-        public List<string>? Brands { get; set; } // Bu liste kullanılacak
+        public List<string>? Brands { get; set; } 
         public List<string>? Models { get; set; }
 
     }
